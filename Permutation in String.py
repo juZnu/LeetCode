b1065457008bbaa643c77700a315f0abc0f0eb64
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-17 18:10:45 -0400

    problem Optimized

--- Permutation in String.py
+++ Permutation in String.py
@@ -1,18 +1,12 @@
 def checkInclusion( s1, s2):
-    dict_s1 = dict()
-    for i in s1:
-        dict_s1[i] = dict_s1.get(i,0) + 1
-    l = 0
-    r = 0
-    while  r + len(s1) <=len(s2):
-        dict_s2 = dict()
-        for i in range(len(s1)):
-            if not dict_s1.get(s2[r+i],0):
-                break
-            dict_s2[s2[r+i]] = dict_s2.get(s2[r+i],0) + 1
-        if dict_s1 == dict_s2:
+    h1 = 0
+    h2 = 0
+    for i in range(len(s1)):
+        h1 += hash(s1[i])*ord(s1[i])
+        h2 += hash(s2[i])*ord(s2[i])
+    for i in range(len(s1),len(s2)):
+        if h1 == h2:
             return True
-        r =l+1
-        l += 1
-    return False
+        h2 += hash(s2[i])*ord(s2[i]) - (hash(s2[i -len(s1)]) * ord(s2[i - len(s1)]))
+    return h1 == h2
 
