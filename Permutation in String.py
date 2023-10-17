6c353c395fde714c1b5f8719473ac3d143f33dba
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-16 23:13:46 -0400

    problem solved but not efficient

--- Permutation in String.py
+++ Permutation in String.py
@@ -4,16 +4,15 @@ def checkInclusion( s1, s2):
         dict_s1[i] = dict_s1.get(i,0) + 1
     l = 0
     r = 0
-    while r - l < len(s1) :
-        if dict_s1.get(s2[r],0):
-            dict_s1[s2[r]] -= 1
-            r += 1
-            continue
-        while l <r :
-            dict_s1[s2[l]] = dict_s1.get(s2[l],0) + 1
-            l += 1
-    return dict_s1
-            
-        
+    while  r + len(s1) <=len(s2):
+        dict_s2 = dict()
+        for i in range(len(s1)):
+            if not dict_s1.get(s2[r+i],0):
+                break
+            dict_s2[s2[r+i]] = dict_s2.get(s2[r+i],0) + 1
+        if dict_s1 == dict_s2:
+            return True
+        r =l+1
+        l += 1
+    return False
 
-print(checkInclusion( "adc",  "dcda"))
