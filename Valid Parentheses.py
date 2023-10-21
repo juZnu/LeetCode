561b1d40fe920b26e60dbb2b6f587cc01273bd30
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-21 17:46:25 -0400

    Valid Parentheses Solved

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,2 +1,17 @@
-def isValid( s):
-    pass
+def isValid(s):
+    dict_ = {
+        '{' : '}',
+        "[" : ']',
+        '(' : ')'
+    }
+    stack = []
+    for i in s:
+        if dict_.get(i,0):
+            stack.append(i)
+        elif stack and dict_.get(stack[-1],0) == i:
+            stack.pop()
+        else:
+            return False
+    return not stack
+        
+print(isValid('({[({[{]})]})'))     
