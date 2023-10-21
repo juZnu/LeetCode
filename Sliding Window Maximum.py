45980ea3ec3eb79ed083f2505b11a4b4148eee56
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-10-21 11:17:40 -0400

    Sliding Window Completed

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -1,3 +1,14 @@
+from collections import deque
 def maxSlidingWindow(nums, k):
-    
-        
+    deque_ = deque()
+    result = []
+    for i in range(len(nums)):
+        while deque_ and deque_[-1] <nums[i]:
+            deque_.pop()
+        deque_.append(nums[i])
+        if i >= k-1:
+            result.append(deque_[0])
+            if nums[i-k+1] == deque_[0]:
+                deque_.popleft()
+    return result
+                
