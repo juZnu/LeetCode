748fc288eb4da3d1d3c440ed5dacbdc4737ac625
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-11-08 20:19:47 -0500

    Daily Temperatures Done

--- Daily Temperatures.py
+++ Daily Temperatures.py
@@ -1,7 +1,12 @@
 def dailyTemperatures(temperatures):
     stack = []
-    for 
-    
+    result = [0] * len(temperatures)
+    for index, value in enumerate(temperatures):
+        while stack and value > stack[-1][0]:
+            pop_value,pop_index = stack.pop()
+            result[pop_index] = index - pop_index
+        stack.append((value,index))
+    return result
             
         
         
