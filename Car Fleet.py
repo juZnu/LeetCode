f4f8110b98ea30205343c1abba32322e49b34586
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-11-11 01:55:17 -0500

    car fleet

--- Car Fleet.py
+++ Car Fleet.py
@@ -1,1 +1,9 @@
 def carFleet(target, position, speed):
+    pair = [(p, s) for p, s in zip(position, speed)]
+    pair.sort(reverse=True)
+    stack = []
+    for p, s in pair:  # Reverse Sorted Order
+        stack.append((target - p) / s)
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
+    return len(stack)
