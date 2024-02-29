269d107788006284863f69536dd1bb6fad4969e8
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2024-02-29 00:53:33 -0800

    Merge Intervals Done

---
+++ Merge Intervals.py
@@ -0,0 +1,9 @@
+def merge( intervals):
+    intervals.sort(key = lambda x:x[0])
+    result = [intervals[0]]
+    for start,end in intervals[1:]:
+        if start <= result[-1][-1]:
+            result[-1][-1] = max(end,result[-1][-1])
+        else:
+            result.append([start,end])
+    return result
